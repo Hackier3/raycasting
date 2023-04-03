68ff29f71f8c5644bafd0a2ad5efa034113f007b
Filip Klimek <FilipKlimek102@gmail.com>
2023-04-03 20:31:34 +0200

    added movement by direction

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,17 @@
-#include <raylib.h>
+#define PI 3.14159
+#define ORIGIN { (float)playerTexture.width / 2, (float)playerTexture.height / 2 }
 
-void drawMap(int, int);
+#include <raylib.h>
+#include <math.h>
+
+void drawMap(int, int, Texture2D);
 
 const int screenWidth = 800;
 const int screenHeight = 800;
 const int tileSize = 48;
 const int mapWidth = 9;
 const int mapHeight = 7;
+double playerRotationAngle = 0;
 const char map[] =
 "#########"
 "#.......#"
@@ -16,34 +21,40 @@ const char map[] =
 "#.......#"
 "#########";
 
+
 int main() {
     InitWindow(screenWidth, screenHeight, "Raylib map");
 
-    int playerX     = tileSize;
-    int playerY     = tileSize;
-    int playerSpeed = 4;
+    float playerX = tileSize;
+    float playerY = tileSize;
+    float playerSpeed = 1.5;
+    Texture2D playerTexture = LoadTexture("images/circle.png");
 
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) && map[(playerY / tileSize) * mapWidth + (playerX / tileSize) + 1] == '.' && map[((playerY + tileSize - 1) / tileSize) * mapWidth + (playerX / tileSize) + 1] == '.') {
-            playerX += playerSpeed;
+        if ((IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))) {
+            playerRotationAngle += 1.5;
         }
-        if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && map[(playerY / tileSize) * mapWidth + ((playerX - 1) / tileSize)] == '.' && map[((playerY + tileSize - 1) / tileSize) * mapWidth + ((playerX - 1) / tileSize)] == '.') {
-            playerX -= playerSpeed;
+        if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))) {
+            playerRotationAngle -= 1.5;
         }
-        if ((IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) && map[(playerY / tileSize) * mapWidth + (playerX / tileSize) + mapWidth] == '.' && map[(playerY / tileSize) * mapWidth + ((playerX + tileSize - 1) / tileSize) + mapWidth] == '.') {
-            playerY += playerSpeed;
+        if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W))) {
+            float dx = cos(playerRotationAngle * PI / 180.0);
+            float dy = sin(playerRotationAngle * PI / 180.0);
+
+            float finalPlayerX = playerX + dx;
+            float finalPlayerY = playerY + dy;
+            
+            playerX = playerX + (finalPlayerX - playerX) * playerSpeed;
+            playerY = playerY + (finalPlayerY - playerY) * playerSpeed;
         }
-        if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && map[((playerY - 1) / tileSize) * mapWidth + (playerX / tileSize)] == '.' && map[((playerY - 1) / tileSize) * mapWidth + ((playerX + tileSize - 1)/ tileSize)] == '.') {
-            playerY -= playerSpeed;
-        }
-        
+
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         // Draw map
-        drawMap(playerX, playerY);
+        drawMap(playerX, playerY, playerTexture);
 
         EndDrawing();
     }
@@ -52,7 +63,8 @@ int main() {
     return 0;
 }
 
-void drawMap(int playerX, int playerY) {
+void drawMap(int playerX, int playerY, Texture2D playerTexture) {
+
     for (int y = 0; y < mapHeight; y++) {
         for (int x = 0; x < mapWidth; x++) {
             if (map[y * mapWidth + x] == '#') {
@@ -67,5 +79,10 @@ void drawMap(int playerX, int playerY) {
             DrawLine(x * tileSize + 1, y * tileSize, x * tileSize + 1, y * tileSize + tileSize, GRAY);
         }
     }
-    DrawRectangle(playerX, playerY, tileSize, tileSize, BLUE);
+
+    // Draw player
+    Rectangle arg2 = { 0, 0, playerTexture.width, playerTexture.height };
+    Rectangle arg3 = { playerX + tileSize / 2, playerY + tileSize / 2, playerTexture.width, playerTexture.height };
+
+    DrawTexturePro(playerTexture, arg2, arg3, ORIGIN, playerRotationAngle, WHITE);
 }
